f2cd0017f8efb6007afd0fbb5eb970958b30d476
pjabie <pjabie@gauchocode.com>
2025-08-05 17:39:11 -0300

    Mejoras al Padron A5

--- l10n_ar_afipws/models/res_partner.py
+++ l10n_ar_afipws/models/res_partner.py
@@ -5,89 +5,171 @@
 
 from odoo import fields, models, api, _
 from odoo.exceptions import UserError
-try:
-    from pysimplesoap.client import SoapFault
-except ImportError:
-    SoapFault = None
 import logging
+import html
+
 _logger = logging.getLogger(__name__)
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    last_update_census = fields.Date(
-        string='Last update census'
+    mipyme_required = fields.Boolean(
+        string="Must credit invoice",
     )
+    mipyme_from_amount = fields.Float(
+        string="Credit invoice from amount",
+    )
+    last_update_census = fields.Date(string="Last update census")
 
+
+    def unescape_dict(self, data):
+        if isinstance(data, dict):
+            return {k: self.unescape_dict(v) for k, v in data.items()}
+        elif isinstance(data, list):
+            return [self.unescape_dict(v) for v in data]
+        elif isinstance(data, str):
+            return html.unescape(data)
+        else:
+            return data
     # Separo esto para poder heredar de otros
     # modulos y extender los datos
-    def parce_census_vals(self, census):
+    def parse_census_vals(self, padron):
+        """
+        Procesa los datos del padrón AFIP y devuelve un diccionario
+        de valores para actualizar el res.partner.
+        """
+        for attr, value in padron.__dict__.items():
+            if isinstance(value, (dict, list, str)):
+                setattr(padron, attr, self.unescape_dict(value))
 
-        # porque imp_iva activo puede ser S o AC
-        imp_iva = census.imp_iva
-        if imp_iva == 'S':
-            imp_iva = 'AC'
-        elif imp_iva == 'N':
-            # por ej. monotributista devuelve N
-            imp_iva = 'NI'
 
+        # Normalización del valor de IVA
+        imp_iva = padron.imp_iva
+        if imp_iva == "S":
+            imp_iva = "AC"
+        elif imp_iva == "N":
+            imp_iva = "NI"
+
+        # Generación inicial de valores
         vals = {
-            'name': census.denominacion,
-            'street': census.direccion,
-            'city': census.localidad,
-            'zip': census.cod_postal,
-            'imp_iva_padron': imp_iva,
-            'last_update_census': fields.Date.today(),
+            "name": padron.denominacion,
+            # Descomentar estos si se necesitan como campos adicionales:
+            # 'name': padron.tipo_persona,
+            # 'name': padron.tipo_doc,
+            # 'name': padron.dni,
+            "estado_padron": padron.estado,
+            "street": padron.direccion,
+            "city": padron.localidad,
+            "zip": padron.cod_postal,
+            "actividades_padron": self.env["afip.activity"].search(
+                [("code", "in", padron.actividades)]
+            ).ids,
+            "impuestos_padron": self.env["afip.tax"].search(
+                [("code", "in", padron.impuestos)]
+            ).ids,
+            "imp_iva_padron": imp_iva,
+            # 'imp_ganancias_padron': padron.imp_ganancias,  # Aún no funcional
+            "monotributo_padron": padron.monotributo,
+            "actividad_monotributo_padron": padron.actividad_monotributo,
+            "empleador_padron": True if padron.empleador == "S" else False,
+            "integrante_soc_padron": padron.integrante_soc,
+            "last_update_padron": fields.Date.today(),
         }
 
-        # padron.idProvincia
+        # --- Provincia y Estado ---
+        _logger.debug("provincia: %s", padron.provincia)
+        _logger.info(padron.data)
 
-        ganancias_inscripto = [10, 11]
-        ganancias_exento = [12]
-        if set(ganancias_inscripto) & set(census.impuestos):
-            vals['imp_ganancias_padron'] = 'AC'
-        elif set(ganancias_exento) & set(census.impuestos):
-            vals['imp_ganancias_padron'] = 'EX'
-        elif census.monotributo == 'S':
-            vals['imp_ganancias_padron'] = 'NC'
-        else:
-            _logger.info(
-                "We couldn't get impuesto a las ganancias from padron, you"
-                "must set it manually")
+        
+        if padron.provincia:
+            caba_codes = ["C", "CABA", "ABA"]
+            state = None
 
-        if census.provincia:
-            # depending on the database, caba can have one of this codes
-            caba_codes = ['C', 'CABA', 'ABA']
-            # if not localidad then it should be CABA.
-            if not census.localidad:
-                state = self.env['res.country.state'].search([
-                    ('code', 'in', caba_codes),
-                    ('country_id.code', '=', 'AR')], limit=1)
-            # If localidad cant be caba
+            if not padron.localidad:
+                # Si no hay localidad, se asume CABA
+                state = self.env["res.country.state"].search(
+                    [("code", "in", caba_codes), ("country_id.code", "=", "AR")], limit=1
+                )
             else:
-                state = self.env['res.country.state'].search([
-                    ('name', 'ilike', census.provincia),
-                    ('code', 'not in', caba_codes),
-                    ('country_id.code', '=', 'AR')], limit=1)
-            if state:
-                vals['state_id'] = state.id
+                # Provincia enviada por AFIP (puede venir sin acentos)
+                provincia_afip = padron.data.get("domicilioFiscal", {}).get("descripcionProvincia", "").strip()
 
-        if imp_iva == 'NI' and census.monotributo == 'S':
-            vals['l10n_ar_afip_responsibility_type_id'] = self.env.ref(
-                'l10n_ar.res_RM').id
-        elif imp_iva == 'AC':
-            vals['l10n_ar_afip_responsibility_type_id'] = self.env.ref(
-                'l10n_ar.res_IVARI').id
-        elif imp_iva == 'EX':
-            vals['l10n_ar_afip_responsibility_type_id'] = self.env.ref(
-                'l10n_ar.res_IVAE').id
+                # Provincias que necesitan corrección de acento
+                # Persona del futuro, te preguntaras porque esta bestialidad?
+                # La respuesta es simple, las provincias estan guardadas con tilde en odoo
+                # Pero afip en su divina sabiduria las devuelve sin tilde
+
+                if provincia_afip == "CORDOBA":
+                    provincia_afip = "Córdoba"
+                elif provincia_afip == "ENTRE RIOS":
+                    provincia_afip = "Entre Ríos"
+                elif provincia_afip == "NEUQUEN":
+                    provincia_afip = "Neuquén"
+                elif provincia_afip == "RIO NEGRO":
+                    provincia_afip = "Río Negro"
+                elif provincia_afip == "TUCUMAN":
+                    provincia_afip = "Tucumán"
+
+                _logger.info("provincia_afip: %s", provincia_afip)
+
+                # Búsqueda en Odoo con el nombre corregido
+                state = self.env["res.country.state"].search(
+                    [
+                        ("name", "ilike", provincia_afip),
+                        ("code", "not in", caba_codes),
+                        ("country_id.code", "=", "AR"),
+                    ],
+                    limit=1,
+                )
+
+            if state:
+                vals["state_id"] = state.id
+
+
+        _logger.info("impuesto y monotributo: %s , %s", imp_iva, padron.monotributo)
+        # --- Responsabilidad ante AFIP ---
+        if imp_iva == "NI" and padron.monotributo == "S":
+            # Responsable Monotributo
+            vals["l10n_ar_afip_responsibility_type_id"] = self.env.ref("l10n_ar.res_RM").id
+
+        elif imp_iva == "NI" and padron.monotributo == "N":
+            # Consumidor Final
+            vals["l10n_ar_afip_responsibility_type_id"] = self.env.ref("l10n_ar.res_CF").id
+
+        elif imp_iva == "AC":
+            # Responsable Inscripto
+            vals["l10n_ar_afip_responsibility_type_id"] = self.env.ref("l10n_ar.res_IVARI").id
+
+        elif imp_iva == "EX":
+            # Exento
+            vals["l10n_ar_afip_responsibility_type_id"] = self.env.ref("l10n_ar.res_IVAE").id
+        
+        elif imp_iva == "NA":
+            # Exento
+            vals["l10n_ar_afip_responsibility_type_id"] = self.env.ref("l10n_ar.res_IVA_NO_ALC").id
+            
         else:
             _logger.info(
-                "We couldn't infer the AFIP responsability from padron, you"
-                "must set it manually.")
+                "We couldn't infer the AFIP responsability from padron, you must set it manually."
+            )
+
+        # --- Impuesto a las ganancias (comentado por ahora) ---
+        # ganancias_inscripto = [10, 11]
+        # ganancias_exento = [12]
+        # if set(ganancias_inscripto) & set(padron.impuestos):
+        #     vals["imp_ganancias_padron"] = "AC"
+        # elif set(ganancias_exento) & set(padron.impuestos):
+        #     vals["imp_ganancias_padron"] = "EX"
+        # elif padron.monotributo == "S":
+        #     vals["imp_ganancias_padron"] = "NC"
+        # else:
+        #     _logger.info(
+        #         "We couldn't get impuesto a las ganancias from padron, you must set it manually."
+        #     )
 
         return vals
+
 
     def get_data_from_padron_afip(self):
         self.ensure_one()
@@ -100,32 +182,41 @@ class ResPartner(models.Model):
         env_type = company._get_environment_type()
         try:
             certificate = company.get_key_and_certificate(
-                company._get_environment_type())
+                company._get_environment_type()
+            )
         except Exception:
-            certificate = self.env['afipws.certificate'].search([
-                ('alias_id.type', '=', env_type),
-                ('state', '=', 'confirmed'),
-            ], limit=1)
+            certificate = self.env["afipws.certificate"].search(
+                [
+                    ("alias_id.type", "=", env_type),
+                    ("state", "=", "confirmed"),
+                ],
+                limit=1,
+            )
             if not certificate:
-                raise UserError(_(
-                    'Not confirmed certificate found on database'))
+                raise UserError(_("Not confirmed certificate found on database"))
             company = certificate.alias_id.company_id
 
         # consultamos a5 ya que extiende a4 y tiene validez de constancia
-        padron = company.get_connection('ws_sr_constancia_inscripcion').connect()
+        padron = company.get_connection("ws_sr_constancia_inscripcion").connect()
         error_msg = _(
-            'No pudimos actualizar desde padron afip al partner %s (%s).\n'
-            'Recomendamos verificar manualmente en la página de AFIP.\n'
-            'Obtuvimos este error: %s')
+            "No pudimos actualizar desde padron afip al partner %s (%s).\n"
+            "Recomendamos verificar manualmente en la página de AFIP.\n"
+            "Obtuvimos este error: %s"
+        )
         try:
             padron.Consultar(cuit)
-        except SoapFault as e:
-            raise UserError(error_msg % (self.name, cuit, e.faultstring))
         except Exception as e:
             raise UserError(error_msg % (self.name, cuit, e))
 
-        if not padron.denominacion or padron.denominacion == ', ':
-            raise UserError(error_msg % (
-                self.name, cuit, 'La afip no devolvió nombre'))
-        vals = self.parce_census_vals(padron)
+        if not padron.denominacion or padron.denominacion == ", ":
+            raise UserError(error_msg % (self.name, cuit, "La afip no devolvió nombre"))
+        vals = self.parse_census_vals(padron)
         return vals
+
+    def l10n_ar_afipws_fe_min_ammount(self):
+        for record in self:
+            if record.l10n_ar_vat:
+                ws = self.env.user.company_id.get_connection("wsfecred").connect()
+                res = ws.ConsultarMontoObligadoRecepcion(record.l10n_ar_vat)
+                record.mipyme_required = True if ws.Resultado == "S" else False
+                record.mipyme_from_amount = float(res)
--- l10n_ar_afipws/wizard/res_partner_update_from_padron_wizard.py
+++ l10n_ar_afipws/wizard/res_partner_update_from_padron_wizard.py
@@ -2,71 +2,110 @@ from odoo import models, api, fields, _
 from ast import literal_eval
 from odoo.exceptions import UserError
 import logging
+
 _logger = logging.getLogger(__name__)
 
 
 class ResPartnerUpdateFromPadronField(models.TransientModel):
-    _name = 'res.partner.update.from.padron.field'
-    _description = 'AFIP A5 Census Field'
+    _name = "res.partner.update.from.padron.field"
+    _description = "AFIP A5 Census Field"
 
     wizard_id = fields.Many2one(
-        'res.partner.update.from.padron.wizard',
-        'Wizard',
+        "res.partner.update.from.padron.wizard",
+        "Wizard",
     )
-    field = fields.Char(
-        'name'
-    )
-    old_value = fields.Char(
-        'old Value'
-    )
-    new_value = fields.Char(
-        'new Value'
-    )
+    field = fields.Char("name")
+    old_value = fields.Char("old Value")
+    new_value = fields.Char("new Value")
+    new_value_display = fields.Char(string="New Value (Display)", compute="_compute_value_display")
+    old_value_display = fields.Char(string="Old Value (Display)", compute="_compute_value_display")
+
+    @api.depends('field', 'new_value', 'old_value')
+    def _compute_value_display(self):
+        for rec in self:
+            # Diccionario de modelos según el campo
+            model_map = {
+                'state_id': 'res.country.state',
+                'l10n_ar_afip_responsibility_type_id': 'l10n_ar.afip.responsibility.type',
+            }
+
+            # --- Procesar old_value ---
+            if rec.field in model_map:
+                try:
+                    # Buscar y mostrar el nombre para old_value
+                    record_old = self.env[model_map[rec.field]].browse(int(rec.old_value))
+                    rec.old_value_display = record_old.name if record_old.exists() else ''
+                except (ValueError, TypeError):
+                    rec.old_value_display = ''
+            else:
+                rec.old_value_display = rec.old_value  # Mostrar directamente si no es Many2one
+
+            # --- Procesar new_value ---
+            if rec.field in model_map:
+                try:
+                    # Buscar y mostrar el nombre para new_value
+                    record_new = self.env[model_map[rec.field]].browse(int(rec.new_value))
+                    rec.new_value_display = record_new.name if record_new.exists() else ''
+                except (ValueError, TypeError):
+                    rec.new_value_display = ''
+            else:
+                rec.new_value_display = rec.new_value  # Mostrar directamente si no es Many2one
+
+
 
 
 class ResPartnerUpdateFromPadronWizard(models.TransientModel):
-    _name = 'res.partner.update.from.padron.wizard'
-    _description = 'AFIP A5 Census Wizard'
+    _name = "res.partner.update.from.padron.wizard"
+    _description = "AFIP A5 Census Wizard"
 
     @api.model
     def get_partners(self):
         # TODO deberiamos buscar de otro manera estos partners
         domain = [
-            ('vat', '!=', False),
-            ('l10n_latam_identification_type_id.l10n_ar_afip_code', '=', 80),
+            ("vat", "!=", False),
+            ("l10n_latam_identification_type_id.l10n_ar_afip_code", "=", 80),
         ]
-        active_ids = self._context.get('active_ids', [])
+        active_ids = self._context.get("active_ids", [])
         if active_ids:
-            domain.append(('id', 'in', active_ids))
-        return self.env['res.partner'].search(domain)
+            domain.append(("id", "in", active_ids))
+        return self.env["res.partner"].search(domain)
 
     @api.model
     def default_get(self, fields):
-        res = super(ResPartnerUpdateFromPadronWizard, self).default_get(
-            fields)
+        res = super(ResPartnerUpdateFromPadronWizard, self).default_get(fields)
         context = self._context
-        if (
-                context.get('active_model') == 'res.partner' and
-                context.get('active_ids')
-        ):
+        if context.get("active_model") == "res.partner" and context.get("active_ids"):
             partners = self.get_partners()
             if not partners:
-                raise UserError(_(
-                    'No se encontró ningún partner con CUIT para actualizar'))
+                raise UserError(
+                    _("No se encontró ningún partner con CUIT para actualizar")
+                )
             elif len(partners) == 1:
-                res['state'] = 'selection'
-                res['partner_id'] = partners[0].id
+                res["state"] = "selection"
+                res["partner_id"] = partners[0].id
         return res
 
     @api.model
     def _get_domain(self):
         fields_names = [
             'name',
+            'estado_padron',
             'street',
             'city',
             'zip',
+            'actividades_padron',
+            'impuestos_padron',
+            'imp_iva_padron',
+            'state_id',
+            'imp_ganancias_padron',
+            'monotributo_padron',
+            'actividad_monotributo_padron',
+            'empleador_padron',
+            'integrante_soc_padron',
+            'last_update_padron',
+            'afip_responsability_type_id',
             'l10n_ar_afip_responsibility_type_id',
-            'last_update_census'
+            # 'constancia',
         ]
         return [
             ('model', '=', 'res.partner'),
@@ -75,67 +114,70 @@ class ResPartnerUpdateFromPadronWizard(models.TransientModel):
 
     @api.model
     def _get_default_title_case(self):
-        parameter = self.env['ir.config_parameter'].sudo().get_param(
-            'use_title_case_on_padron_afip')
-        if parameter == 'False' or parameter == '0':
+        parameter = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("use_title_case_on_padron_afip")
+        )
+        if parameter == "False" or parameter == "0":
             return False
         return True
 
     @api.model
     def get_fields(self):
-        return self.env['ir.model.fields'].search(self._get_domain())
+        return self.env["ir.model.fields"].search(self._get_domain())
 
-    state = fields.Selection([
-        ('option', 'Option'),
-        ('selection', 'Selection'),
-        ('finished', 'Finished')],
-        'State',
+    state = fields.Selection(
+        [("option", "Option"), ("selection", "Selection"), ("finished", "Finished")],
+        "State",
         readonly=True,
         required=True,
-        default='option',
+        default="option",
     )
     field_ids = fields.One2many(
-        'res.partner.update.from.padron.field',
-        'wizard_id',
-        string='Fields',
+        "res.partner.update.from.padron.field",
+        "wizard_id",
+        string="Fields",
     )
     partner_ids = fields.Many2many(
-        'res.partner',
-        'partner_update_from_padron_rel',
-        'update_id', 'partner_id',
-        string='Partners',
+        "res.partner",
+        "partner_update_from_padron_rel",
+        "update_id",
+        "partner_id",
+        string="Partners",
         default=get_partners,
     )
     partner_id = fields.Many2one(
-        'res.partner',
-        string='Partner',
+        "res.partner",
+        string="Partner",
         readonly=True,
     )
     update_constancia = fields.Boolean(
         default=True,
     )
     title_case = fields.Boolean(
-        string='Title Case',
-        help='Converts retreived text fields to Title Case.',
+        string="Title Case",
+        help="Converts retreived text fields to Title Case.",
         default=_get_default_title_case,
     )
     field_to_update_ids = fields.Many2many(
-        'ir.model.fields',
-        'res_partner_update_fields',
-        'update_id', 'field_id',
-        string='Fields To Update',
-        help='Only this fields are going to be retrived and updated',
+        "ir.model.fields",
+        "res_partner_update_fields",
+        "update_id",
+        "field_id",
+        string="Fields To Update",
+        help="Only this fields are going to be retrived and updated",
         default=get_fields,
         domain=_get_domain,
         required=True,
     )
 
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def change_partner(self):
         self.ensure_one()
         self.field_ids.unlink()
         partner = self.partner_id
-        fields_names = self.field_to_update_ids.mapped('name')
+        fields_names = self.field_to_update_ids.mapped("name")
         if partner:
             partner_vals = partner.get_data_from_padron_afip()
             lines = []
@@ -143,34 +185,62 @@ class ResPartnerUpdateFromPadronWizard(models.TransientModel):
             for key in fields_names:
                 old_value = partner[key]
                 new_value = partner_vals[key]
-                if new_value == '':
+                if new_value == "":
                     new_value = False
-                if self.title_case and key in ('name', 'city', 'street'):
+                if self.title_case and key in ("name", "city", "street"):
                     new_value = new_value and new_value.title()
-                if key in ('impuestos_padron', 'actividades_padron'):
+                if key in ("impuestos_padron", "actividades_padron"):
                     old_value = old_value.ids
-                elif key in ('state_id', 'l10n_ar_afip_responsibility_type_id'):
+                elif key in ("state_id", "l10n_ar_afip_responsibility_type_id"):
                     old_value = old_value.id
-                if new_value and key in fields_names and \
-                        old_value != new_value:
+                if new_value and key in fields_names and old_value != new_value:
                     line_vals = {
-                        'wizard_id': self.id,
-                        'field': key,
-                        'old_value': old_value,
-                        'new_value': new_value or False,
+                        "wizard_id": self.id,
+                        "field": key,
+                        "old_value": old_value,
+                        "new_value": new_value or False,
                     }
                     lines.append((0, False, line_vals))
             self.field_ids = lines
 
+
+
     def _update(self):
         self.ensure_one()
         vals = {}
+
         for field in self.field_ids:
-            if field.field in ('impuestos_padron', 'actividades_padron'):
-                vals[field.field] = [(6, False, literal_eval(field.new_value))]
+            _logger.info(f"Procesando campo: {field.field} con valor: {field.new_value}")
+
+            if field.field in ("impuestos_padron", "actividades_padron"):
+                # Many2many con comando (6, False, ids)
+                valores = [(6, False, literal_eval(field.new_value))]
+                vals[field.field] = valores
+                _logger.info(f"Asignando M2M {field.field}: {valores}")
+
+            elif field.field == 'state_id':
+                # Buscar el estado (provincia)
+                #Totalmente ineficiente pero funciona TODO:Arreglarlo
+                provincia = self.env['res.country.state'].browse(int(field.new_value))
+                if provincia.exists():
+                    vals['state_id'] = provincia.id  # Guardar ID en vals
+                    _logger.info(f"Provincia encontrada: {provincia.name} (ID: {provincia.id})")
+                else:
+                    _logger.warning(f"Provincia con ID {field.new_value} no encontrada.")
+
+            elif field.field == 'l10n_ar_afip_responsibility_type_id':
+                # Many2one simple
+                vals['l10n_ar_afip_responsibility_type_id'] = int(field.new_value)
+                _logger.info(f"Asignando responsabilidad AFIP: {field.new_value}")
+
             else:
+                # Otros campos simples
                 vals[field.field] = field.new_value
+                _logger.info(f"Asignando campo simple {field.field}: {field.new_value}")
+
+        _logger.info(f"Valores finales a escribir en partner {self.partner_id.id}: {vals}")
         self.partner_id.write(vals)
+
 
     def automatic_process_cb(self):
         for partner in self.partner_ids:
@@ -178,63 +248,65 @@ class ResPartnerUpdateFromPadronWizard(models.TransientModel):
             self.change_partner()
             self._update()
 
-        self.write({'state': 'finished'})
+        self.write({"state": "finished"})
         return {
-            'type': 'ir.actions.act_window',
-            'res_model': self._name,
-            'res_id': self.id,
-            'view_mode': 'form',
-            'target': 'new',
+            "type": "ir.actions.act_window",
+            "res_model": self._name,
+            "res_id": self.id,
+            "view_mode": "form",
+            "target": "new",
         }
 
     def update_selection(self):
         self.ensure_one()
         if not self.field_ids:
-            self.write({'state': 'finished'})
+            self.write({"state": "finished"})
             return {
-                'type': 'ir.actions.act_window',
-                'res_model': self._name,
-                'res_id': self.id,
-                'view_mode': 'form',
-                'target': 'new',
+                "type": "ir.actions.act_window",
+                "res_model": self._name,
+                "res_id": self.id,
+                "view_mode": "form",
+                "target": "new",
             }
         self._update()
         return self.next_cb()
 
     def next_cb(self):
-        """
-        """
+        """ """
         self.ensure_one()
         if self.partner_id:
-            self.write({'partner_ids': [(3, self.partner_id.id, False)]})
+            self.write({"partner_ids": [(3, self.partner_id.id, False)]})
         return self._next_screen()
 
     def _next_screen(self):
         self.ensure_one()
-        self.refresh()
         values = {}
         if self.partner_ids:
             # in this case, we try to find the next record.
             partner = self.partner_ids[0]
-            values.update({
-                'partner_id': partner.id,
-                'state': 'selection',
-            })
+            values.update(
+                {
+                    "partner_id": partner.id,
+                    "state": "selection",
+                }
+            )
         else:
-            values.update({
-                'state': 'finished',
-            })
+            values.update(
+                {
+                    "state": "finished",
+                }
+            )
 
         self.write(values)
         # because field is not changed, view is distroyed and reopen, on change
         # is not called an we call it manually
         self.change_partner()
         return {
-            'type': 'ir.actions.act_window',
-            'res_model': self._name,
-            'res_id': self.id,
-            'view_mode': 'form',
-            'target': 'new',
+            "type": "ir.actions.act_window",
+            "res_model": self._name,
+            "res_id": self.id,
+            "view_mode": "form",
+            "target": "new",
         }
 
     def start_process_cb(self):
